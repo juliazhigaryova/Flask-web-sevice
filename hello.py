878db7d870886207f241874e039f65ac1624b12e
juliazhigaryova <juliazhigaryova@yandex.ru>
2023-09-10 17:38:23 +0300

    add post request, add bad request

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify,abort,redirect, url_for
 from joblib import dump, load
 import numpy as np
 
@@ -20,14 +20,33 @@ if __name__ == '__main__':
 
 @app.route('/iris/<param>')
 def iris(param):
-    param = param.split(',')
-    param = [float(num) for num in param]
-    param = np.array(param).reshape(1, -1)
-    predict = knn.predict(param)
+    pred = predict(param)
     iris_class = {1: 'setosa', 2: 'versicolor', 3: 'virginica'}
-    name_class = iris_class.get(int(predict))
+    name_class = iris_class.get(int(pred))
     return show_image(name_class)
 
 @app.route('/show_image')
 def show_image(name):
     return '<img src="/static/' + name +'.jpg" alt="IRIS flower">'
+
+@app.route('/badrequest400')
+def bad_request():
+    return abort(400)
+
+@app.route('/iris_post', methods=['POST'])
+def add_message():
+
+    try:
+        content = request.get_json()
+        pred_val = predict(content["flower"])
+        pred = {'class': str(pred_val[0])}
+    except:
+        return redirect(url_for('bad_request'))
+
+    return jsonify(pred)
+
+def predict(param):
+    param = param.split(',')
+    param = [float(num) for num in param]
+    param = np.array(param).reshape(1, -1)
+    return knn.predict(param)
