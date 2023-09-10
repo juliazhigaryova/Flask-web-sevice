a3ab6aa6571432609182094df9edfc66b6682d30
juliazhigaryova <juliazhigaryova@yandex.ru>
2023-09-10 15:56:06 +0300

    add model pkl file, add show pred values with pictures

--- hello.py
+++ hello.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from joblib import dump, load
+import numpy as np
 
 app = Flask(__name__)
+knn = load('knn.pkl')
 
 @app.route("/")
 def hello_world():
     print('Hey there!')
-    return "<h1>Hello, my friend!</h1>"
+    return "<h1>Hello, friend!</h1>"
 
 @app.route('/user/<username>')
 def profile(username):
@@ -13,3 +16,18 @@ def profile(username):
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0')
+
+
+@app.route('/iris/<param>')
+def iris(param):
+    param = param.split(',')
+    param = [float(num) for num in param]
+    param = np.array(param).reshape(1, -1)
+    predict = knn.predict(param)
+    iris_class = {1: 'setosa', 2: 'versicolor', 3: 'virginica'}
+    name_class = iris_class.get(int(predict))
+    return show_image(name_class)
+
+@app.route('/show_image')
+def show_image(name):
+    return '<img src="/static/' + name +'.jpg" alt="IRIS flower">'
