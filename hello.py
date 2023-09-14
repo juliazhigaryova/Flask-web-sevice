2a19a9a421488e2c6023c886a5e9de3510beaaf2
juliazhigaryova <juliazhigaryova@yandex.ru>
2023-09-14 16:35:28 +0300

    upload file pred, get file via post

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify,abort,redirect, url_for,render_template
+from flask import Flask, request, jsonify,abort,redirect, url_for,render_template,send_file
 from joblib import dump, load
 import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 knn = load('knn.pkl')
@@ -54,7 +55,7 @@ def predict(param):
 from flask_wtf import FlaskForm
 from wtforms import StringField,FileField
 from wtforms.validators import DataRequired
-from werkzeug.utils import secure_filename
+
 import os
 
 app.config.update(dict(
@@ -71,11 +72,61 @@ def submit():
     form = MyForm()
     if form.validate_on_submit():
         f = form.file.data
-        filename = form.name.data + '.txt'
-        f.save(os.path.join(
-            filename
-        ))
-        return (str(form.name))
+        filename = form.name.data + '.csv'
+        # f.save(os.path.join(
+        #     filename
+        # ))
+
+        df = pd.read_csv(f, header=None)
+        pred = knn.predict(df)
+        result = pd.DataFrame(pred)
+        result.to_csv(filename,index=False)
+
+        return send_file(filename,
+                         mimetipe='text/csv',
+                         attachment_filename=filename,
+                         as_attachment=True)
 
     return render_template('submit.html', form=form)
+
+    import os
+    from flask import Flask, flash, request, redirect, url_for
+    from werkzeug.utils import secure_filename
+    from werkzeug.utils import safe_str_cmp
+
+    UPLOAD_FOLDER = ''
+    ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+    def allowed_file(filename):
+        return '.' in filename and \
+               filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    @app.route('/upload', methods=['GET', 'POST'])
+    def upload_file():
+        if request.method == 'POST':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # If the user does not select a file, the browser submits an
+            # empty file without a filename.
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename + 'uploaded')
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return 'file uploaded'
+        return '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method=post enctype=multipart/form-data>
+          <input type=file name=file>
+          <input type=submit value=Upload>
+        </form>
+        '''
 
